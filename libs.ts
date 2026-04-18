0347e627f09ebfea321de5b34445cf51aee454b3
Snazzah <me@snazzah.com>
2026-04-18 12:51:00 -0400

    Update dysnomia to show E2EE support

--- libs.ts
+++ libs.ts
@@ -1027,7 +1027,7 @@ export const libs: Lib[] = [
 		language: 'JavaScript',
 		apiVer: 10,
 		gwVer: 10,
-		voiceVer: 8,
+		voiceVer: 'E2EE',
 		slashCommands: 'Yes',
 		buttons: 'Yes',
 		selectMenus: 'Yes',
